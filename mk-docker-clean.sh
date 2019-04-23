0bf4ac571efadf8176ca1f60b9bebfc2434c8962
oiramario <oiramario@gmail.com>
2019-04-23 14:16:40 +0800

    bug fix

--- mk-docker-clean.sh
+++ mk-docker-clean.sh
@@ -2,7 +2,7 @@
 #
 #set -x
 
-if [ ! $1 = "all" ]; then
+if [ "$1" != "all" ]; then
     docker stop $(docker ps -a | grep "Exited" | awk '{print $1 }')
     docker rm $(docker ps -a | grep "Exited" | awk '{print $1 }')
     docker rmi $(docker images | grep "none" | awk '{print $3}')

e2210454539579f30c6973c1e15e9f27c7acad82
oiram <oiram@mario.localdomain>
2019-08-14 05:04:57 +0800

    tiny step for qemu

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -7,13 +7,13 @@
 #    -initrd ${DISTRO}/ramdisk.cpio.gz \
 #    -append "root=/dev/ram rdinit=/init console=ttyAMA0"
 
-dd if=${BUILD}/qemu-4.0.0/roms/u-boot/u-boot.bin of=/tmp/disk.img conv=notrunc bs=1M count=32
-dd if=${DISTRO}/boot.scr of=/tmp/disk.img conv=notrunc bs=1 seek=2M
-dd if=${DISTRO}/fitImage.itb of=/tmp/disk.img conv=notrunc bs=1 seek=4M
+#dd if=${BUILD}/qemu-4.0.0/roms/u-boot/u-boot.bin of=/tmp/mmc.img conv=notrunc bs=1M
+#dd if=${DISTRO}/boot.scr of=/tmp/mmc.img conv=notrunc bs=1M seek=2
+#dd if=${DISTRO}/fitImage.itb of=/tmp/mmc.img conv=notrunc bs=1M seek=4
 
 ${BUILD}/qemu-4.0.0/aarch64-softmmu/qemu-system-aarch64 \
     -serial stdio \
     -machine virt -cpu cortex-a57 -smp 4 -m 1024 \
     -bios ${BUILD}/qemu-4.0.0/roms/u-boot/u-boot.bin \
-    -drive if=none,file=/tmp/disk.img,media=disk,id=boot \
+    -drive if=none,file=/tmp/mmc.img,media=disk,id=boot \
     -device virtio-blk-device,drive=boot

3ff3d00d8a30ffa9a22661c8bf9d1168dfb71b10
oiram <oiram@mario.localdomain>
2019-08-23 23:39:45 +0800

    fix qemu warning

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -17,5 +17,5 @@ ${QEMU}/aarch64-softmmu/qemu-system-aarch64 \
     -cpu cortex-a57 -smp 4 \
     -m 2G,slots=2,maxmem=4G \
     -bios ${DISTRO}/qemu-u-boot.bin \
-    -drive if=none,file=${DISTRO}/qemu-boot.img,id=hd0 \
+    -drive if=none,file=${DISTRO}/qemu-boot.img,format=raw,id=hd0 \
     -device virtio-blk-device,drive=hd0
